477a47ea95ce80b15a655455c7a87404e44e3851
mo2men5aled <momenk208@gmail.com>
2023-04-26 05:59:50 +0200

    update modal rusability and background shadow

--- src/modals/modal.js
+++ src/modals/modal.js
@@ -3,29 +3,32 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 
-const CustomModal = ({ children, header }) => {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
-
+const CustomModal = ({
+  children,
+  header,
+  FooterChildren,
+  ButtonIcon,
+  handleClose,
+  handleShow,
+  show,
+}) => {
   return (
     <>
-      <Button variant="dark" onClick={handleShow}>
-        Update
+      <Button variant="link dark" onClick={handleShow}>
+        {ButtonIcon}
       </Button>
 
       <Modal
         show={show}
         onHide={handleClose}
         size="md"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
+        aria-labelledby="example-modal-sizes-title-sm"
       >
         <Modal.Header closeButton>
-          <Modal.Title>{header}</Modal.Title>
+          <Modal.Title id="example-modal-sizes-title-sm">{header}</Modal.Title>
         </Modal.Header>
         <Modal.Body>{children}</Modal.Body>
+        <Modal.Footer>{FooterChildren}</Modal.Footer>
       </Modal>
     </>
   );
