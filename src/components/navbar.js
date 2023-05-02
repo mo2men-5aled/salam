2b24fe4048dd02ab46d264a9fee02a522d069bdb
mo2men5aled <momenk208@gmail.com>
2023-05-02 08:27:11 +0300

    add image items for the articles and admins

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Image from "react-bootstrap/Image";
-import { Button, Spinner } from "react-bootstrap";
+import { Button, Nav, Spinner } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 import logo from "../assets/white_logo.png";
@@ -32,58 +32,69 @@ function NavBar() {
       <Navbar key="sm" bg="dark" expand="sm" variant="dark">
         <Container>
           <Navbar.Brand href="/" className="">
-            <Image src={logo} alt="logo" width="35" height="35" roundedCircle />
-          </Navbar.Brand>
-          <Navbar.Brand href="/" className="me-auto">
+            <Image src={logo} alt="logo" width="35" height="35" roundedCircle />{" "}
             Salam
           </Navbar.Brand>
 
-          <div className="d-flex justify-content-end">
-            {user === undefined && !name ? (
-              <Spinner animation="border" size="sm" variant="light" />
-            ) : (
-              <div>
-                {user === null ? (
-                  <Link
-                    as={Button}
-                    to="/user/login"
-                    className="me-2 btn btn-outline-light"
-                    variant="dark"
-                  >
-                    Login
-                  </Link>
-                ) : (
-                  <>
-                    <div>
-                      <Navbar.Text
-                        className="me-3"
-                        style={{
-                          color: "white",
-                          fontSize: "1 rem",
-                          cursor: "pointer",
-                        }}
-                        onClick={() => {
-                          window.location.href = "/user/profile/" + user.uid;
-                        }}
-                      >
-                        {user.displayName ? user.displayName : name}
-                      </Navbar.Text>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link
+                onClick={() => {
+                  window.location.href = "/articles";
+                }}
+              >
+                Artilces
+              </Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+            </Nav>
+            <div className="d-flex justify-content-end">
+              {user === undefined && !name ? (
+                <Spinner animation="border" size="sm" variant="light" />
+              ) : (
+                <div>
+                  {user === null ? (
+                    <Link
+                      as={Button}
+                      to="/user/login"
+                      className="me-2 btn btn-outline-light"
+                      variant="dark"
+                    >
+                      Login
+                    </Link>
+                  ) : (
+                    <>
+                      <div>
+                        <Navbar.Text
+                          className="me-3"
+                          style={{
+                            color: "white",
+                            fontSize: "1 rem",
+                            cursor: "pointer",
+                          }}
+                          onClick={() => {
+                            window.location.href = "/user/profile/" + user.uid;
+                          }}
+                        >
+                          {user.displayName ? user.displayName : name}
+                        </Navbar.Text>
 
-                      <Button
-                        className="me-2"
-                        variant="outline-light"
-                        onClick={() => {
-                          auth.signOut();
-                        }}
-                      >
-                        Logout
-                      </Button>
-                    </div>
-                  </>
-                )}
-              </div>
-            )}
-          </div>
+                        <Button
+                          className="me-2"
+                          variant="outline-light"
+                          onClick={() => {
+                            auth.signOut();
+                          }}
+                        >
+                          Logout
+                        </Button>
+                      </div>
+                    </>
+                  )}
+                </div>
+              )}
+            </div>
+          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
